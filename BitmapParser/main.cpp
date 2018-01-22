9c31ade0bf10265b8b122900e9899af257108203
Pavilion Sahota <pavilion.sahota@borwell.com>
2018-01-22 13:47:55 +0000

    I finally got it working, after 4 days of sheer hardwork and learning C++ as fast as I possibly can. As you could tell, I couldn't wait to show my line manager the tremendous and excellent code that I have produced. Do you know what he said? "You got 2 more tasks". And so on that day, Pavilion overdosed on tea to try and tackle the new tasks...

--- BitmapParser/main.cpp
+++ BitmapParser/main.cpp
@@ -1,10 +1,13 @@
 #include "stdafx.h"
 #include "BitmapParser.h"
+#include <stdio.h>
 
 int main(int argc, _TCHAR* argv[])
 {
-	std::string filename("lel3.bmp");
-	//std::string filename2("aaaaaaAAAAAAAAAAHHHH.bmp");
+	
+	std::string filename("chad.bmp");
+	std::string filename2("aaaaaaAAAAAAAAAAHHHH.bmp");
+	remove(filename2.c_str());
 	BitmapParser* parser = new BitmapParser(filename);
 	//BitmapParser* create = new BitmapParser(filename2);
 	
@@ -16,7 +19,6 @@ int main(int argc, _TCHAR* argv[])
 	
 	delete parser;
 	//delete create;
-
 	//Have the lambs stopped screaming Clarice?
 	return 0;
 }
