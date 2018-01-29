c9008623f2a37bd31a89d59d4e0fb48a9f78e9ac
Pavilion Sahota <pavilionsahota@gmail.com>
2018-01-29 16:08:43 +0000

    Minor changes

--- BitmapParser/main.cpp
+++ BitmapParser/main.cpp
@@ -8,7 +8,11 @@
 // #$pla$h Gang
 int main(int argc, _TCHAR* argv[])
 {
-
+	std::cout << "               []===============================================================[]               " << std::endl;
+	std::cout << R"(  /M\         <|| /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//\/\/\/\/\/\ ||>         /M\  )" << std::endl;
+	std::cout << "<|[o]|00===00<<|| ++++++++++++++++ - Welcome to BitmapParser - ++++++++++++++++ ||>>00===00|[o]|>" << std::endl;
+	std::cout << R"(  \W/         <|| \/\/\/\/\/\/\/\/\/\/\/\/\\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/ ||>         \W/  )" << std::endl;
+	std::cout << "               []===============================================================[]               " << std::endl;
 	std::string path = R"(C:\Users\pavilion.sahota\Desktop\input\)";
 	std::string filename;
 	std::cout << "Please enter the filename (including file extension): ";
