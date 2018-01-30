27a3cee992d1d0219922eeb33e17e5062af16e32
Pavilion Sahota <pavilionsahota@gmail.com>
2018-01-30 17:15:03 +0000

    Bug fixes 2

--- BitmapParser/main.cpp
+++ BitmapParser/main.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 
 // #$pla$h Gang
-void justForFun() { while (true) { std::cout << "Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  " << '\r';std::this_thread::sleep_for(std::chrono::milliseconds(100));std::cout << "emes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  M" << '\r';std::this_thread::sleep_for(std::chrono::milliseconds(100));std::cout << "mes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Me" << '\r';std::this_thread::sleep_for(std::chrono::milliseconds(100));std::cout << "es  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Mem" << '\r';std::this_thread::sleep_for(std::chrono::milliseconds(100));std::cout << "s  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Meme" << '\r';std::this_thread::sleep_for(std::chrono::milliseconds(100));std::cout << "  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes" << '\r';std::this_thread::sleep_for(std::chrono::milliseconds(100));std::cout << " Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes " << '\r';std::this_thread::sleep_for(std::chrono::milliseconds(100)); } }
+//void justForFun() { while (true) { std::cout << "Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  " << '\r';std::this_thread::sleep_for(std::chrono::milliseconds(100));std::cout << "emes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  M" << '\r';std::this_thread::sleep_for(std::chrono::milliseconds(100));std::cout << "mes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Me" << '\r';std::this_thread::sleep_for(std::chrono::milliseconds(100));std::cout << "es  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Mem" << '\r';std::this_thread::sleep_for(std::chrono::milliseconds(100));std::cout << "s  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Meme" << '\r';std::this_thread::sleep_for(std::chrono::milliseconds(100));std::cout << "  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes" << '\r';std::this_thread::sleep_for(std::chrono::milliseconds(100));std::cout << " Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes  Memes " << '\r';std::this_thread::sleep_for(std::chrono::milliseconds(100)); } }
 
 
 int main(int argc, _TCHAR* argv[])
@@ -26,7 +26,14 @@ int main(int argc, _TCHAR* argv[])
 	remove(filename2.c_str());
 	BitmapParser* parser = new BitmapParser(filename);
 	delete parser;
-  justForFun();
+	int b = 10;
+	for (int i = 1;i<11;i++)
+	{
+		std::cout << "BitmapParser will terminate in: " << b << "  " << '\r';
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		b--;
+	}
+	//justForFun();
 	//Have the lambs stopped screaming Clarice?
 	return 0;
 }
